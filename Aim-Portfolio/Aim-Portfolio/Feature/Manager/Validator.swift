5fe09c55a04339c94992af47ae6fbe061e91fc76
ericKwon95 <ericyong95@gmail.com>
2024-12-10 16:53:03 +0900

    feat: 비밀번호 검증 로직 구현

--- Aim-Portfolio/Aim-Portfolio/Feature/Manager/Validator.swift
+++ Aim-Portfolio/Aim-Portfolio/Feature/Manager/Validator.swift
@@ -27,6 +27,23 @@ enum ValidationResult {
             }
         }
     }
+    
+    enum Password {
+        case valid
+        case tooShort
+        case tooLong
+        case invalidCharacter
+        
+        var description: String {
+            switch self {
+            case .valid:
+                return "사용할 수 있는 비밀번호 입니다."
+            case .tooShort:
+                return "비밀번호는 10자 이상이어야 합니다."
+            case .tooLong:
+                return "비밀번호는 20자를 넘을 수 없습니다."
+            case .invalidCharacter:
+                return "비밀번호는 영문, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
             }
         }
     }
@@ -44,6 +61,23 @@ struct Validator {
         
         let alphabets = /[A-Za-z]+/
         guard id.wholeMatch(of: alphabets) != nil else {
+            return .invalidCharacter
+        }
+        
+        return .valid
+    }
+    
+    static func validatePassword(_ password: String) -> ValidationResult.Password {
+        guard password.count >= 10 else {
+            return .tooShort
+        }
+        
+        guard password.count <= 20 else {
+            return .tooLong
+        }
+        
+        let alphabetNumberSpecialCharacters = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};:\'"\\|,.<>\/?])[\w!@#$%^&*()_+\-=\[\]{};:\'"\\|,.<>\/?]+$/
+        guard password.wholeMatch(of: alphabetNumberSpecialCharacters) != nil else {
             return .invalidCharacter
         }
         
