ef253563ead14783ef13c3487488b473efb30691
ericKwon95 <ericyong95@gmail.com>
2024-12-10 10:10:10 +0900

    feat: initial commit

---
+++ Aim-Portfolio/Aim-Portfolio/Aim_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aim_PortfolioApp.swift
+//  Aim-Portfolio
+//
+//  Created by 권승용 on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Aim_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aim-Portfolio/Aim-PortfolioTests/Aim_PortfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aim_PortfolioTests.swift
+//  Aim-PortfolioTests
+//
+//  Created by 권승용 on 12/10/24.
+//
+
+import Testing
+@testable import Aim_Portfolio
+
+struct Aim_PortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
