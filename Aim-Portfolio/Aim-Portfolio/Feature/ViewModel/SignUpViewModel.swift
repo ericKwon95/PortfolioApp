52e996aba91e4d60b41d7e4bcf9f78c0d7e15567
ericKwon95 <ericyong95@gmail.com>
2024-12-10 20:10:21 +0900

    feat: 전화번호 검증 및 포맷 기능 구현
    - 유저가 숫자 입력 시 010-1234-5678 형태로 자동 포맷팅

--- Aim-Portfolio/Aim-Portfolio/Feature/ViewModel/SignUpViewModel.swift
+++ Aim-Portfolio/Aim-Portfolio/Feature/ViewModel/SignUpViewModel.swift
@@ -10,8 +10,12 @@ import Foundation
 final class SignUpViewModel: ObservableObject {
     @Published var id = ""
     @Published var idValidationResult = " "
+    
     @Published var password = ""
     @Published var passwordValidationResult = " "
+    
+    @Published var phoneNumber = "010-"
+    @Published var phoneNumberValidationResult = " "
     
     func validateNewID(_ newID: String) {
         let trimmedID = newID.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -25,5 +29,30 @@ final class SignUpViewModel: ObservableObject {
         let validation = Validator.validatePassword(trimmedPassword)
         passwordValidationResult = validation.description
         password = trimmedPassword
+    }
+    
+    func validateNewPhoneNumber(_ newPhoneNumber: String) {
+        let spaceTrimmed = newPhoneNumber.trimmingCharacters(in: .whitespacesAndNewlines)
+        let hypenTrimmed = spaceTrimmed.replacingOccurrences(of: "-", with: "")
+        let validation = Validator.validatePhoneNumber(hypenTrimmed)
+        phoneNumberValidationResult = validation.description
+        let formattedPhoneNumber = makePhoneNumberFormatted(hypenTrimmed)
+        phoneNumber = formattedPhoneNumber
+    }
+}
+
+private extension SignUpViewModel {
+    func makePhoneNumberFormatted(_ phoneNumber: String) -> String {
+        var stringWithHypen = phoneNumber
+        
+        if stringWithHypen.count > 3 {
+            stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
+        }
+        
+        if stringWithHypen.count > 8 {
+            stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: 8 - stringWithHypen.count))
+        }
+        
+        return stringWithHypen
     }
 }
