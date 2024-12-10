df96f1e872ff43f152a76d213b4cc557e5857082
ericKwon95 <ericyong95@gmail.com>
2024-12-10 16:31:53 +0900

    feat: 아이디 유효성 검사 기능 구현

---
+++ Aim-Portfolio/Aim-Portfolio/Feature/Manager/Validator.swift
@@ -0,0 +1,49 @@
+//
+//  Validator.swift
+//  Aim-Portfolio
+//
+//  Created by 권승용 on 12/10/24.
+//
+
+import Foundation
+
+enum ValidationResult {
+    enum ID {
+        case valid
+        case tooShort
+        case tooLong
+        case invalidCharacter
+        
+        var description: String {
+            switch self {
+            case .valid:
+                return "사용할 수 있는 ID 입니다."
+            case .tooShort:
+                return "ID는 7자 이상이어야 합니다."
+            case .tooLong:
+                return "ID는 20자를 넘을 수 없습니다."
+            case .invalidCharacter:
+                return "ID는 영문자만 포함할 수 있습니다."
+            }
+        }
+    }
+}
+
+struct Validator {
+    static func validateID(_ id: String) -> ValidationResult.ID {
+        guard id.count >= 7 else {
+            return .tooShort
+        }
+        
+        guard id.count <= 20 else {
+            return .tooLong
+        }
+        
+        let alphabets = /[A-Za-z]+/
+        guard id.wholeMatch(of: alphabets) != nil else {
+            return .invalidCharacter
+        }
+        
+        return .valid
+    }
+}
