7b403d24559177611c016abd488e7785e22f8ff5
ericKwon95 <ericyong95@gmail.com>
2024-12-12 15:03:28 +0900

    feat: 에셋 타입별 이름 및 비율을 표시해주는 AssetRatioView 구현
    - baseColor로부터 인덱스에 따라 brightness를 낮춰주는 형식으로 각 에셋마다의 컬러값 구현

---
+++ Aim-Portfolio/Aim-Portfolio/Source/View/Common/AssetRatioView.swift
@@ -0,0 +1,52 @@
+//
+//  AssetRatioView.swift
+//  Aim-Portfolio
+//
+//  Created by 권승용 on 12/12/24.
+//
+
+import SwiftUI
+
+struct AssetRatioView: View {
+    let type: AssetType
+    let assets: [AssetItem]
+    let baseColor: Color
+    
+    init(type: AssetType, assets: [AssetItem], baseColor: Color) {
+        self.type = type
+        self.assets = assets
+        self.baseColor = baseColor
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(type.description)
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundStyle(.assetTypeTitle)
+                Spacer()
+            }
+            ForEach(Array(zip(assets.indices, assets)), id: \.0) { index, asset in
+                assetInfo(asset, color: baseColor, brightness: Double(index) * -0.1)
+            }
+        }
+    }
+    
+    private func assetInfo(_ asset: AssetItem, color: Color, brightness: Double) -> some View {
+        HStack(spacing: 0) {
+            Text(asset.name)
+                .font(.system(size: 15, weight: .regular))
+                .foregroundStyle(.white)
+                .frame(width: 100, alignment: .leading)
+                .padding(.trailing, 16)
+            RatioBar(ratio: asset.ratio, color: color, brightness: brightness)
+            Spacer()
+            Text("\(String(format: "%.2f", asset.ratio))%")
+                .font(.system(size: 14, weight: .regular))
+                .frame(width: 70, alignment: .trailing)
+                .foregroundStyle(color)
+                .monospacedDigit()
+                .brightness(brightness)
+        }
+    }
+}
