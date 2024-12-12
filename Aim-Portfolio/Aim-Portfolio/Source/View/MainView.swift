1c19f0e76ec52957040ed5b504b69311446f363b
ericKwon95 <ericyong95@gmail.com>
2024-12-12 15:04:08 +0900

    feat: 메인 화면 구현
    - 파이 차트 미구현

---
+++ Aim-Portfolio/Aim-Portfolio/Source/View/MainView.swift
@@ -0,0 +1,47 @@
+//
+//  MainView.swift
+//  Aim-Portfolio
+//
+//  Created by 권승용 on 12/11/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @StateObject private var viewModel = PortfolioViewModel()
+    
+    var body: some View {
+        ZStack {
+            Color.appBackground
+                .ignoresSafeArea()
+            
+            VStack(spacing: 32) {
+                AssetRatioView(
+                    type: .stock,
+                    assets: viewModel.stocks,
+                    baseColor: .stock
+                )
+                
+                AssetRatioView(
+                    type: .bond,
+                    assets: viewModel.bonds,
+                    baseColor: .bond
+                )
+                
+                AssetRatioView(
+                    type: .etc,
+                    assets: viewModel.etc,
+                    baseColor: .etc
+                )
+            }
+            .padding()
+        }
+        .onAppear {
+            viewModel.loadAssetItems()
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
